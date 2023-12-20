46827d8830b9fec00970f0d05d9b23d88f043644
Lucas Priestley <lmpriestley@outlook.com>
2023-12-20 13:49:43 -0500

    Fix issue with cargo now being able to find readme on linux.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![allow(non_camel_case_types)]
 #![allow(non_upper_case_globals)]
 #![allow(non_snake_case)]
-#![doc = include_str!("../README.md")]
+#![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/README.md"))]
 
 #[doc(hidden)]
 mod ffi;
